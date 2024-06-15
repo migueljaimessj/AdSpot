22092d0a0fe380b9a8fa66e35cc4b14457c5e31d
Miguel Jaimes <migueljaimes@sjnative.com>
2024-06-15 18:35:19 +0200

    Initial Commit

---
+++ AdSpot/AdSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdSpotApp.swift
+//  AdSpot
+//
+//  Created by Miguel Jaimes on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AdSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
